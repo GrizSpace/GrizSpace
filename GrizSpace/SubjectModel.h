98edc01554990b3b323ab0ba118360413de1fa34
Jaylene <jaylene.naylor@gmail.com>
2012-03-17 17:23:25 -0600

    New database file GrizSpaceDB.sqlite added with updated tables and additional tables for I'm Bored and Study Buddy.  Added SubjectModel.  Changed DBAccess to point to new DB file

---
+++ GrizSpace/SubjectModel.h
@@ -0,0 +1,20 @@
+//
+//  SubjectModel.h
+//  GrizSpace
+//
+//  Created by Jaylene Naylor on 3/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubjectModel : NSObject{
+    //create properties for each column in the subject table
+    NSString *idSubject;
+    
+}
+
+@property (nonatomic, retain)NSString *idSubject;
+
+
+@end
---
+++ GrizSpace/SubjectModel.m
@@ -0,0 +1,14 @@
+//
+//  SubjectModel.m
+//  GrizSpace
+//
+//  Created by Jaylene Naylor on 3/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SubjectModel.h"
+
+@implementation SubjectModel
+
+@synthesize idSubject;
+@end
