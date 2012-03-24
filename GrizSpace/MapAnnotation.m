327fb9d288882d66605494d8db87ae187c41d16b
Kevin Scott <kevin.scott@umconnect.umt.edu>
2012-03-23 22:05:12 -0600

    adding annotation arrived back in for merge.

--- GrizSpace/MapAnnotation.m
+++ GrizSpace/MapAnnotation.m
@@ -52,7 +52,7 @@
         [self setTitle: tmpBM.name];
         [self setSubtitle: tmpBM.idBuilding];  
         [self setRadius:tmpBM.Radius];
-//        [self setArrived:false];
+        [self setArrived:false];
     }
     return self;
 }
