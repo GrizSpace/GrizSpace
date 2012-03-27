857eec6d47c063e8248bb06620e675624a947b93
William Lyon <lyonwj@gmail.com>
2012-03-27 14:47:01 -0600

    storyboard and static changes for proj3

--- GrizSpace/CourseListViewController.m
+++ GrizSpace/CourseListViewController.m
@@ -57,7 +57,7 @@
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    //self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)viewDidUnload
